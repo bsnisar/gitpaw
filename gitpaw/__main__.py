9ffef6734e1aefd0952d42640efa41845e418cec
bsnisar <bogdan.sns@gmail.com>
2023-09-29 19:27:40 +0200

    next changes

---
+++ gitpaw/__main__.py
@@ -0,0 +1,14 @@
+# pylint: disable=missing-function-docstring
+# pylint: disable=broad-exception-caught
+# pylint: disable=anomalous-backslash-in-string
+
+import argparse
+import asyncio
+
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
